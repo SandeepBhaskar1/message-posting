a800a0a40e0a9a27a18cd7d29cbe936911e1d177
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2025-02-16 16:57:42 +0530

    dh

--- backend/multer.js
+++ backend/multer.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const uploadsDir = path.join(__dirname, '/tmp', 'uploads');
+const uploadsDir = '/tmp/uploads';
 
 if (!fs.existsSync(uploadsDir)) {
     fs.mkdirSync(uploadsDir, { recursive: true });
