999b5da38f9767c0a1e770e36b7bd0d4b8fa1e6f
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2025-02-16 16:28:01 +0530

    fir

--- backend/multer.js
+++ backend/multer.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const uploadsDir = path.join(__dirname, 'public', 'uploads');
+const uploadsDir = path.join(__dirname, '/tmp', 'uploads');
 
 if (!fs.existsSync(uploadsDir)) {
     fs.mkdirSync(uploadsDir, { recursive: true });
@@ -9,7 +9,7 @@ if (!fs.existsSync(uploadsDir)) {
 
 const profilePicStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './public/uploads');
+        cb(null, uploadsDir);
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
